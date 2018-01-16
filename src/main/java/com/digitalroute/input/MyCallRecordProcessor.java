708af3747ff6c628f7295e4d7eeb249b33081649
Fredrik Hed <fredrik.hed@digitalroute.com>
2018-01-16 19:02:21 +0100

    still no implementation

---
+++ src/main/java/com/digitalroute/input/MyCallRecordProcessor.java
@@ -0,0 +1,10 @@
+package com.digitalroute.input;
+
+import java.io.InputStream;
+
+public class MyCallRecordProcessor implements CallRecordsProcessor {
+    @java.lang.Override
+    public void processBatch(InputStream in) {
+
+    }
+}
---
+++ src/test/java/com.digitalroute/TheTest.java
@@ -0,0 +1,4 @@
+package com.digitalroute;
+
+public class TheTest {
+}
